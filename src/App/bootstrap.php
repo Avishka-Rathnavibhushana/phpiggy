26b7a9b065d659b64f7917020e5b72e79c9187c8
“Avishka-Rathnavibhushana” <mailavishka@gmail.com>
2023-08-10 15:22:24 +0530

    Implement new about page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,11 +6,12 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
 
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 // HomeController::class is the same as 'App\Controllers\HomeController'
 // In php  we can call classes and methods as strings
