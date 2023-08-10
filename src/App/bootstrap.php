73cae8429e54b905b62e288a3d24144faa344698
“Avishka-Rathnavibhushana” <mailavishka@gmail.com>
2023-08-10 22:19:26 +0530

    Move Routes paths to a separate file called Routes

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,14 +6,10 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
 
-use App\Controllers\{HomeController, AboutController};
+use function App\Config\registerRoutes;
 
 $app = new App();
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
-
-// HomeController::class is the same as 'App\Controllers\HomeController'
-// In php  we can call classes and methods as strings
+registerRoutes($app);
 
 return $app;
