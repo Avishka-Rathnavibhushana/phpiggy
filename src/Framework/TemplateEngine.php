4708a0e6fbf72a13f055384f2a3a56fc4f2ff4a1
“Avishka-Rathnavibhushana” <mailavishka@gmail.com>
2023-08-10 14:56:33 +0530

    Create an output buffer

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -13,6 +13,13 @@ class TemplateEngine
     public function render(String $template, array $data = [])
     {
         extract($data, EXTR_SKIP);
+
+        ob_start();
+
         include "{$this->basePath}/{$template}";
+
+        $output = ob_get_clean();
+
+        return $output;
     }
 }
