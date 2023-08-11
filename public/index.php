f3fb461b2a6227cf0b35445deeb8c1db94cc99e1
“Avishka-Rathnavibhushana” <mailavishka@gmail.com>
2023-08-11 11:01:20 +0530

    Add error log on UI

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+// Remove this line in production
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//
 
 include __DIR__ . "/../src/App/functions.php";
 
