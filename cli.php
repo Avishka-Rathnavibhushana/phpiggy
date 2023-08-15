2186a29cd915f766b106cfda2c8079dc02e09a6f
“Avishka-Rathnavibhushana” <mailavishka@gmail.com>
2023-08-15 09:27:50 +0530

    Refactor DB Connection as a class

--- cli.php
+++ cli.php
@@ -1,18 +1,18 @@
 <?php
 
-echo "Hello World!";
+include __DIR__ . '/src/Framework/Database.php';
 
-$driver = 'mysql';
-$config = http_build_query(data: [
-    'host' => '127.0.0.1', // need to use this instead of localhost
-    'port' => 3308,
-    'dbname' => 'phpiggy',
-], arg_separator: ';');
+use Framework\Database;
 
-$dsn = "{$driver}:{$config}";
-$username = 'root';
-$password = '';
-
-$db = new PDO($dsn, $username, $password);
+$db = new Database(
+    'mysql',
+    [
+        'host' => '127.0.0.1', // need to use this instead of localhost
+        'port' => 3308,
+        'dbname' => 'phpiggy',
+    ],
+    'root',
+    ''
+);
 
 echo "Connected to database!";
